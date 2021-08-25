ada860e5b218a89d8c8b9edfcacedc1f9e6c305c
Khushank <47456301+Khushank1120@users.noreply.github.com>
2021-08-25 18:43:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/invoiceapp_master/MainActivity2.java
@@ -0,0 +1,14 @@
+package com.example.invoiceapp_master;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity2 extends MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+    }
+}
